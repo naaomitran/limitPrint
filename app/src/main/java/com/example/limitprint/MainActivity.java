4095c981e9d853793ab033106401a1bb59ab63de
s0l2b <anyima@students.cs.ubc.ca>
2020-12-06 15:44:29 -0800

    GPS integration and progress bar implementation

--- app/src/main/java/com/example/limitprint/MainActivity.java
+++ app/src/main/java/com/example/limitprint/MainActivity.java
@@ -1,18 +1,37 @@
 package com.example.limitprint;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private ProgressBar progressBar;
+
+    private LocationManager locationManager;
+    private Location lastLocation;
+    private float[] tempDistance = new float[1];
+    private float sum ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +47,32 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        Button button = (Button) findViewById(R.id.start);
+        button = (Button) findViewById(R.id.start);
+        textView = (TextView) findViewById(R.id.textView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setMax(20000);
+        progressBar.setProgress(0);
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (button.getText().equals("Start")) {
+                if (button.getText().equals("start")) {
                     button.setText("Clicked!");
-
+                    locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, new LocationListener() {
+                        @Override
+                        public void onLocationChanged(@NonNull Location location) {
+                            if (lastLocation != null) {
+                                Location.distanceBetween(lastLocation.getLatitude(), lastLocation.getLongitude(), location.getLatitude(), location.getLongitude(), tempDistance);
+                                    sum += tempDistance[0];
+                                    progressBar.setProgress((int)sum);
+                                textView.setText(Float.toString(sum));
+                            }
+                            lastLocation = location;
+                        }
+                    });
                 }
             }
         });

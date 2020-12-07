db62aff131ed4d00751dcedf352dbf54ced4dd2e
s0l2b <anyima@students.cs.ubc.ca>
2020-12-06 17:15:49 -0800

    Added Notifications and made code easier to read

--- app/src/main/java/com/example/limitprint/MainActivity.java
+++ app/src/main/java/com/example/limitprint/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.limitprint;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +20,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -27,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
     private LocationManager locationManager;
     private Location lastLocation;
     private float[] tempDistance = new float[1];
-    private float sum ;
+    private float sum;
+    private LocationListener locationListener;
+    private boolean[] notifArray = {false, false, false};
+    private NotificationManagerCompat notificationManagerCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,36 +51,89 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.start);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setMax(20000);
+        progressBar.setMax(10);
         progressBar.setProgress(0);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (button.getText().equals("start")) {
-                    button.setText("Clicked!");
-                    locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                    }
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, new LocationListener() {
-                        @Override
-                        public void onLocationChanged(@NonNull Location location) {
-                            if (lastLocation != null) {
-                                Location.distanceBetween(lastLocation.getLatitude(), lastLocation.getLongitude(), location.getLatitude(), location.getLongitude(), tempDistance);
-                                    sum += tempDistance[0];
-                                    progressBar.setProgress((int)sum);
-                            }
-                            lastLocation = location;
-                        }
-                    });
-                }
+        locationListener = locationListenerInit();
+
+        button.setOnClickListener(buttonInit());
+
+        createNotificationChannels();
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+    }
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(
+                    "channel1",
+                    "channel1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("This is Channel 1");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+        }
+    }
+
+    private LocationListener locationListenerInit() {
+        return location -> {
+            if (lastLocation != null) {
+                Location.distanceBetween(lastLocation.getLatitude(), lastLocation.getLongitude(), location.getLatitude(), location.getLongitude(), tempDistance);
+                sum += tempDistance[0];
+                progressBar.setProgress((int) sum);
             }
-        });
+            lastLocation = location;
+            if (sum > (float) progressBar.getMax() / 2 && !notifArray[0]) {
+                notifArray[0] = true;
+                Notification notification = new NotificationCompat.Builder(this, "channel1")
+                        .setSmallIcon(R.drawable.wind)
+                        .setContentTitle("(CO2)unter Alert")
+                        .setContentText("You have driven over 50% of a sustainable carbon footprint!")
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                        .build();
+                notificationManagerCompat.notify(1,notification);
+            }
+            if (sum > (float) progressBar.getMax() / (float) (4/3) && !notifArray[1]) {
+                notifArray[1] = true;
+                Notification notification = new NotificationCompat.Builder(this, "channel1")
+                        .setSmallIcon(R.drawable.wind)
+                        .setContentTitle("(CO2)unter Alert")
+                        .setContentText("You have driven over 75% of a sustainable carbon footprint! Try and minimize your driving time and maximize other carbon efficient methods!")
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                        .build();
+                notificationManagerCompat.notify(2,notification);
+            }
+            if (sum > (float) progressBar.getMax() && !notifArray[2]) {
+                notifArray[2] = true;
+                Notification notification = new NotificationCompat.Builder(this, "channel1")
+                        .setSmallIcon(R.drawable.wind)
+                        .setContentTitle("(CO2)unter Alert")
+                        .setContentText("You have driven over 100% of a sustainable carbon footprint! Try and minimize your driving time and maximize other carbon efficient methods!")
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                        .build();
+                notificationManagerCompat.notify(3,notification);
+            }
+        };
     }
 
-    public void endTravel(View view) {
+    private View.OnClickListener buttonInit() {
+        return v -> {
+            if (button.getText().equals("start")) {
+                button.setText("end");
+                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+            } else {
+                button.setText("start");
+                locationManager.removeUpdates(locationListener);
+            }
+        };
     }
 
-    public void startTravel(View view) {
-    }
 }
